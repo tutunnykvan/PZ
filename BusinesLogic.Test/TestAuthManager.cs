0b94113f42484c07d40b9b751e38f59d82e26352
tutunnykvan <70881858+tutunnykvan@users.noreply.github.com>
2020-11-25 22:03:06 +0200

    Add files via upload

---
+++ BusinesLogic.Test/TestAuthManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using Moq;
+using Bll.Concrete;
+using DAL.Interface;
+
+namespace BusinesLogic.Test
+{
+    
+        
+    
+        [TestFixture]
+        public class AuthManagerTests
+        {
+            private Mock<IAdminDAL> adminDal;
+            private AuthManager manager;
+
+            [SetUp]
+            public void Setup()
+            {
+                adminDal = new Mock<IAdminDAL>(MockBehavior.Strict);
+
+                manager = new AuthManager(adminDal.Object);
+            }
+
+
+            [Test]
+        public void LoginUserTest()
+            {
+                string username = "user";
+                string password = "pass";
+
+                adminDal.Setup(d => d.Login(username, password)).Returns(true);
+                var res = manager.Login(username, password);
+
+                Assert.IsTrue(res);
+            }
+        }
+    
+   
+}
+
---
+++ BusinesLogic.Test/TestPerson.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using Moq;
+using Bll.Concrete;
+using DAL.Interface;
+using DTO;
+
+
+namespace BusinessLogic.Tests
+{
+    [TestFixture]
+    public class PersonManagerTests
+    {
+     
+        private Mock<IPersonDAL> personDal;
+        private PersonManager person;
+
+        [SetUp]
+        public void Setup()
+        {
+           personDal = new Mock<IPersonDAL>(MockBehavior.Strict);
+
+            person = new PersonManager(personDal.Object);
+        }
+
+
+        [Test]
+        public void AddPersonTest()
+        { 
+
+            PersonDTO inMovie = new PersonDTO
+            {
+                FirstName = "Ivan",
+                LastName = "Husak",
+                Birthday = new DateTime(2000, 7, 1),
+                RoleId = 13
+            };
+            PersonDTO outMovie = new PersonDTO { PersonId = 1 };
+
+            personDal.Setup(d => d.CreatePerson(inMovie)).Returns(outMovie);
+            var res = person.AddPerson(inMovie);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(outMovie.PersonId, res.PersonId);
+        }
+    }
+}
