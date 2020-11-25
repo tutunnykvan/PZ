5fd6d4dfabf037d7e153012ab4f5279310eab3b8
tutunnykvan <70881858+tutunnykvan@users.noreply.github.com>
2020-11-25 22:00:36 +0200

    Add files via upload

---
+++ interfaces/IPersonManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace Bll.interfaces
+{
+   public interface IPersonManager
+    {
+        PersonDTO AddPerson(PersonDTO peroson);
+        List<PersonDTO> GetListOfPerson();
+        PersonDTO UpdatePerson(PersonDTO node);
+        void DeletePerson(int Id);
+    }
+}
